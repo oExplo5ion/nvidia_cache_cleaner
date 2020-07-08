dc52102a173e1c35788ce43c08647e988be9a575
Aleksey Robul <axelvirus@gmail.com>
2020-07-08 19:15:48 +0300

    init commit

---
+++ NVIDIA Cleaner/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace NVIDIA_Cleaner
+{
+    public partial class MainWindow : Window
+    {
+
+        private TextBox logLabel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            logLabel = (TextBox) FindName("LogLabel");
+
+            Button cleanButton = (Button) FindName("CleanButton");
+            cleanButton.Click += cleanButtonClick;
+        }
+
+        private async void cleanButtonClick(object sender, RoutedEventArgs e)
+        {
+            await log("Starting...");
+
+            string[] paths = { Paths.NVCache, Paths.DCache, Paths.GlCache, Paths.Temp };
+
+            foreach (string path in paths)
+            {
+                await clearFolder(path);
+            }
+
+            await log("Done");
+        }
+
+        private async Task log(string text)
+        {
+            logLabel.Text = logLabel.Text + "\n" + text;
+            logLabel.ScrollToEnd();
+        }
+
+        private async Task clearFolder(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                await log("Clearing at: " + path);
+
+                DirectoryInfo di = new DirectoryInfo(path);
+
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    try
+                    {
+                        file.Delete();
+                    }
+                    catch 
+                    {
+                        await log("Delete failed at path: " + file.FullName);
+                    }
+                }
+                foreach (DirectoryInfo dir in di.GetDirectories())
+                {
+                    try
+                    {
+                        dir.Delete(true);
+                    }
+                    catch
+                    {
+                        await log("Delete failed at path: " + dir.FullName);
+                    }
+                }
+            }
+            else
+            {
+                await log("Folder does not exist: " + path);
+            }
+        }
+    }
+}
---
+++ NVIDIA Cleaner/Paths.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Windows.Input;
+
+namespace NVIDIA_Cleaner
+{
+    public static class Paths
+    {
+        // System folders
+        public static string SystemPath
+        {
+            get
+            {
+                return Path.GetPathRoot(Environment.GetFolderPath(Environment.SpecialFolder.System));
+            }
+        }
+
+        public static string AppData
+        {
+            get
+            {
+                string roaming = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                return Directory.GetParent(roaming).FullName;
+            }
+        }
+
+
+        // Cache folders
+        public static string NVCache
+        {
+            get
+            {
+                string programData = Path.Combine(SystemPath, "ProgramData");
+                string nvidiaCorporation = Path.Combine(programData, "NVIDIA Corporation");
+                return Path.Combine(nvidiaCorporation, "NV_Cache");
+            }
+        }
+
+        public static string DCache
+        {
+            get
+            {
+                string local = Path.Combine(AppData, "Local");
+                string dCache = Path.Combine(local, "D3DSCache");
+                return dCache;
+            }
+        }
+
+        public static string GlCache
+        {
+            get 
+            {
+                string local = Path.Combine(AppData, "Local");
+                string nvidia = Path.Combine(local, "NVIDIA");
+                return Path.Combine(nvidia, "GLCache");
+            }
+        }
+
+        public static string Temp
+        {
+            get
+            {
+                string local = Path.Combine(AppData, "Local");
+                return Path.Combine(local, "Temp");
+            }
+        }
+    }
+}
